73eafd337265b864ce69337eb5367fd6c9a9ca62
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-12-23 17:35:31 +0100

    s

--- src/components/SubmitForm/SubmitForm.jsx
+++ src/components/SubmitForm/SubmitForm.jsx
@@ -1,23 +1,21 @@
 import { useState, useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import RenderList from '../RenderList/RenderList';
 import fetchKeyword from '../API/fetchKeyword';
-// =================================================>
+
 const SubmitForm = () => {
   const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
   const [movisNames, setMovisNames] = useState(
     () => JSON.parse(localStorage.getItem('movisNames')) || []
   );
-  // const location = useLocation();
-  // =================================================>
+
+  const movieId = searchParams.get('movieId') ?? '';
+
   useEffect(() => {
     localStorage.setItem('movisNames', JSON.stringify(movisNames));
   }, [movisNames]);
-  // =================================================>
-  const handleInputChange = ({ target: { value } }) => {
-    setQuery(value);
-  };
-  // =================================================>
+
   const handleSubmit = async event => {
     event.preventDefault();
     if (!query.trim()) return;
@@ -25,26 +23,32 @@ const SubmitForm = () => {
     try {
       const response = await fetchKeyword(query);
       console.log('fetchKeyword=====>', response.data.results);
-      setMovisNames(prevState => [...prevState, response.data.results]);
+      setMovisNames(prevState => [...prevState, ...response.data.results]);
     } catch (error) {
       throw error;
     }
     setQuery('');
   };
 
-  // =================================================>
+  const updateQueryString = e => {
+    const movieIdValue = e.target.value;
+    if (movieIdValue === '') {
+      return setSearchParams({});
+    }
+    setSearchParams({ movieId: movieIdValue });
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={query}
-          onChange={handleInputChange}
+          value={movieId}
+          onChange={updateQueryString}
           placeholder="Search movie "
         ></input>
         <button type="submit">Search</button>
       </form>
-
       <RenderList movisNames={movisNames} />
     </>
   );
